95b8dbefb06d6510a0e7d519dc7eb9465941e5a0
HyundongHwang <hhd2002@gmail.com>
2017-02-23 01:01:08 +0900

    최초커밋

---
+++ QrBarCodeReader/MainWindow.xaml.cs
@@ -0,0 +1,154 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Drawing.Imaging;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using WebEye.Controls.Wpf;
+using ZXing;
+
+namespace QrBarCodeReader
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        private WebCameraId _webcamDevice = null;
+
+        public MainWindow()
+        {
+            InitializeComponent();
+            this.Loaded += _This_Loaded;
+            this.Unloaded += _This_Unloaded;
+            this.SizeChanged += _This_SizeChanged;
+            this.CbWebCams.SelectionChanged += _CbWebCams_SelectionChanged;
+            this.BtnRetake.Click += _BtnRetake_Click;
+        }
+
+        private async void _BtnRetake_Click(object sender, RoutedEventArgs e)
+        {
+            await _StartCapture(this.CbWebCams.SelectedValue.ToString());
+        }
+
+        private async void _CbWebCams_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            if (_webcamDevice == null)
+                return;
+
+            await _StartCapture(this.CbWebCams.SelectedValue.ToString());
+        }
+
+        private async void _This_Loaded(object sender, RoutedEventArgs e)
+        {
+            await _StartCapture();
+        }
+
+        private void _This_Unloaded(object sender, RoutedEventArgs e)
+        {
+            try
+            {
+                this.WebEyeWebCameraControl.StopCapture();
+            }
+            catch (Exception ex)
+            {
+                Trace.TraceError(ex.ToString());
+            }
+        }
+
+        private void _This_SizeChanged(object sender, SizeChangedEventArgs e)
+        {
+            _ResizeCameraImgControl();
+        }
+
+        private void _ResizeCameraImgControl()
+        {
+            try
+            {
+                var bm = this.WebEyeWebCameraControl.GetCurrentImage();
+                this.WebEyeWebCameraControl.Height = this.WebEyeWebCameraControl.ActualWidth * bm.Height / bm.Width;
+            }
+            catch (Exception ex)
+            {
+                Trace.TraceError(ex.ToString());
+            }
+        }
+
+        private DateTime _takeStartTime;
+
+        private async Task _StartCapture(string webCamName = null)
+        {
+            this.TbResult.Text = "";
+            this.ImgSnapshot.Source = null;
+            _takeStartTime = DateTime.Now;
+
+            try
+            {
+                this.WebEyeWebCameraControl.StopCapture();
+            }
+            catch (Exception ex)
+            {
+                Trace.TraceError(ex.ToString());
+            }
+
+            var videoDeviceList = this.WebEyeWebCameraControl.GetVideoCaptureDevices();
+
+            if (!videoDeviceList.Any())
+                return;
+
+            var webCamNameList = videoDeviceList.Select(d => d.Name);
+            Trace.TraceInformation(@"webCamNameList : " + JsonConvert.SerializeObject(webCamNameList, Formatting.Indented));
+            this.CbWebCams.ItemsSource = webCamNameList;
+
+            if (string.IsNullOrWhiteSpace(webCamName))
+            {
+                this.CbWebCams.SelectedIndex = 0;
+                _webcamDevice = videoDeviceList.First();
+            }
+            else
+            {
+                _webcamDevice = videoDeviceList.FirstOrDefault(d => d.Name == webCamName);
+            }
+
+            this.WebEyeWebCameraControl.StartCapture(_webcamDevice);
+            _ResizeCameraImgControl();
+
+            var barcodeReader = new BarcodeReader();
+
+            while (true)
+            {
+                await TaskEx.Delay(1000);
+                var bm = this.WebEyeWebCameraControl.GetCurrentImage();
+                var barcodeResult = barcodeReader.Decode(bm);
+
+                if (barcodeResult != null)
+                {
+                    var elapsedTime = DateTime.Now - _takeStartTime;
+                    this.TbResult.Text = $"걸린시간 : {elapsedTime.TotalMilliseconds.ToString("0")}ms \n결과 : {barcodeResult.ToString()}";
+                    System.Windows.Forms.SendKeys.SendWait(barcodeResult.ToString());
+
+                    using (var ms = new MemoryStream())
+                    {
+                        bm.Save(ms, ImageFormat.Bmp);
+                        this.ImgSnapshot.Source = BitmapFrame.Create(ms, BitmapCreateOptions.None, BitmapCacheOption.OnLoad);
+                    }
+
+                    break;
+                }
+            }
+        }
+
+    }
+}
--- README.md
+++ README.md
@@ -1,2 +1,21 @@
-# QrBarCodeReader
-QrBarCodeReader
+# 구현내용
+- 카메라 디바이스에서 캡쳐된 이미지를 분석하여 QRcode BARcode 인식하는 예제
+- 캡쳐가 시작되면 1초에 한번씩 이미지를 분석시도하고 성공할때까지 계속하는 단순한 알고리즘.
+- QRcode 인식은 꽤 좋은 편이고, BARcode 인식은 성능과 성공률이 낮은편 ㅠㅠ
+
+# 데모
+![](http://s24.postimg.org/45xgzlt39/screenshot_51.png)
+
+# 종속성
+- windows xp sp3 이상
+- 닷넷프레임워크 4.0
+    - http://s3.ap-northeast-2.amazonaws.com/coconut-client/dotNetFx40_Full_x86_x64.exe
+- 윈도우업데이트 KB2468871
+    - http://s3.ap-northeast-2.amazonaws.com/coconut-client/NDP40-KB2468871-v2-x86.exe
+
+# 참고자료
+- 코드리더 라이브러리 : 
+    - https://zxingnet.codeplex.com
+- 웹캠 라이브러리 : 
+    - http://www.codeproject.com/Articles/330177/Yet-another-Web-Camera-control
+    - https://www.nuget.org/packages/WebEye.Controls.wpf.WebCameraControl
